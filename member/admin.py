89099783ad16887c993fbb6fbd71eaf68ba7312e
Iain Macdonald <iain.macdonald@williamhill.com>
2017-07-20 11:47:00 +0100

    Updated admin page to use annotation tags to make it easier

--- member/admin.py
+++ member/admin.py
@@ -2,38 +2,45 @@ from django.contrib import admin
 from member.models import BadgeAwards, Badges, BadgeMedia, CoachInstuction, BadgeAssesments, UserMember, TeamManagers
 
 
+@admin.register(BadgeAwards)
 class BadgeAwardAdmin(admin.ModelAdmin):
     list_display = ('userId_id', 'badgeId_id', 'dateAwarded', 'score', 'comments')
 
+pass
 
+
+@admin.register(UserMember)
 class UserMemberAdmin(admin.ModelAdmin):
     list_display = ('user', 'full_name', 'favourite_player', 'favourite_team', 'managerId_id')
+pass
 
 
+@admin.register(BadgeAssesments)
 class BadgeAssessmentsAdmin(admin.ModelAdmin):
     list_display = ('badgeId', 'description')
+pass
 
 
+@admin.register(CoachInstuction)
 class CoachInstuctionAdmin(admin.ModelAdmin):
     list_display = ('id', 'tipType', 'description', 'badgeName')
+pass
 
 
+@admin.register(Badges)
 class BadgesAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'levels')
+pass
 
 
+@admin.register(TeamManagers)
 class TeamManagerAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'team')
+    list_filter = ('full_name', 'team')
+pass
 
 
+@admin.register(BadgeMedia)
 class BadgeMediaAdmin(admin.ModelAdmin):
     list_display = ('badgeId', 'pageUrl', 'description', 'mediaType')
-
-
-admin.site.register(BadgeAwards, BadgeAwardAdmin)
-admin.site.register(UserMember, UserMemberAdmin)
-admin.site.register(Badges, BadgesAdmin)
-admin.site.register(BadgeMedia, BadgeMediaAdmin)
-admin.site.register(CoachInstuction, CoachInstuctionAdmin)
-admin.site.register(BadgeAssesments, BadgeAssessmentsAdmin)
-admin.site.register(TeamManagers, TeamManagerAdmin)
+pass
